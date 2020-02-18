37cf4ffe300b30c6950127aa4ac426ab22928ba3
Nicholas <46433505+NicholasReis@users.noreply.github.com>
2020-02-18 12:55:51 -0800

    Removed uneccessary stuff and created a res folder

--- ImageTest.java
+++ ImageTest.java
@@ -19,26 +19,16 @@ import javafx.scene.image.ImageView;
  */
 public class ImageTest extends Application
 {
-    // We keep track of the count, and label displaying the count:
-    private int count = 0;
-    private Label myLabel = new Label("0");
-    private Image img = new Image("image.png");
-    /**
-     * The start method is the main entry point for every JavaFX application. 
-     * It is called after the init() method has returned and after 
-     * the system is ready for the application to begin running.
-     *
-     * @param  stage the primary stage for this application.
-     */
+    private Image img = new Image("res/horse.png");
+
     @Override
     public void start(Stage stage)
     {
-        // Create a Button or any control item
-        Button myButton = new Button("Count");
         ImageView imgView = new ImageView();
         imgView.setImage(img);
         imgView.setFitHeight(300);
         imgView.setFitWidth(300);
+
         // Create a new grid pane
         GridPane pane = new GridPane();
         pane.setPadding(new Insets(10, 10, 10, 10));
@@ -46,32 +36,15 @@ public class ImageTest extends Application
         pane.setVgap(10);
         pane.setHgap(10);
 
-        //set an action on the button using method reference
-        myButton.setOnAction(this::buttonClick);
-
-        // Add the button and label into the pane
-        pane.add(myLabel, 1, 0);
-        pane.add(myButton, 0, 0);
-        pane.add(imgView, 0, 2);
+        pane.add(imgView, 0, 0);
 
         // JavaFX must have a Scene (window content) inside a Stage (window)
         Scene scene = new Scene(pane, 300,100);
         stage.setTitle("JavaFX Example");
         stage.setScene(scene);
-        stage.setWidth(500);
-        stage.setHeight(400);
+        stage.setWidth(350);
+        stage.setHeight(350);
         // Show the Stage (window)
         stage.show();
-    }
-
-    /**
-     * This will be executed when the button is clicked
-     * It increments the count by 1
-     */
-    private void buttonClick(ActionEvent event)
-    {
-        // Counts number of button clicks and shows the result on a label
-        count = count + 1;
-        myLabel.setText(Integer.toString(count));
     }
 }
