d390f0fe6e21ced81e5bc9cd7abebd8105f1cefb
Nicholas <46433505+NicholasReis@users.noreply.github.com>
2020-02-18 16:22:42 -0800

    Every other line printed. Going to try and put both images into one.

--- ImageTest.java
+++ ImageTest.java
@@ -1,3 +1,5 @@
+import java.awt.image.BufferedImage;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.geometry.Insets;
@@ -5,7 +7,12 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javax.imageio.ImageIO;
+import javafx.scene.image.WritableImage;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
+import java.io.File;
 /**
  * Write a description of JavaFX class ImageTest here.
  *
@@ -14,27 +21,46 @@ import javafx.scene.image.ImageView;
  */
 public class ImageTest extends Application
 {
-    private Image img = new Image("res/horse.png");
-
+    private Image img1 = new Image("res/horse.png");
+    private Image img2 = new Image("res/horse2.png");
     @Override
     public void start(Stage stage)
     {
-        ImageView imgView = new ImageView();
-        imgView.setImage(img);
-        imgView.setFitHeight(300);
-        imgView.setFitWidth(300);
+        double imgH = 300;
+        double imgW = 300;
+        WritableImage wImg = new WritableImage((int)img1.getWidth(), (int)img1.getHeight());
+        PixelReader pR = img1.getPixelReader();
+        PixelReader pR2 = img2.getPixelReader();
+        PixelWriter pW = wImg.getPixelWriter();
+
+        for(int x = 0; x < (int)img1.getWidth(); x++){
+            for(int y = 0; y < (int)img1.getHeight(); y++){
+                if(y % 2 == 0){
+                    pW.setArgb(x, y, pR.getArgb(x,y));
+                }
+            }
+        }
 
         // Create a new grid pane
-        StackPane pane = new StackPane(imgView);
+        ImageView img = new ImageView(wImg);
+        img.setFitHeight(300);
+        img.setFitWidth(300);
+        try {
+            // retrieve image
+            File outputfile = new File("saved.png");
+            BufferedImage bImage = SwingFXUtils.fromFXImage(img.getImage(), null);
+            ImageIO.write(bImage, "png", outputfile);
+        } catch (Exception e) {
+        }
+        StackPane pane = new StackPane(img);
         pane.setPadding(new Insets(10, 10, 10, 10));
-        pane.setMinSize(300, 300);
 
         // JavaFX must have a Scene (window content) inside a Stage (window)
         Scene scene = new Scene(pane, 300,100);
         stage.setTitle("JavaFX Example");
         stage.setScene(scene);
-        stage.setWidth(350);
-        stage.setHeight(350);
+        stage.setWidth(imgW);
+        stage.setHeight(imgH);
         // Show the Stage (window)
         stage.show();
     }
